e7a8ef721d17b73dad04c4227e9ea7aa15a14338
Prahladborah <86541744+Prahladborah@users.noreply.github.com>
2024-06-23 14:59:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from events import on_ready, on_message
+from events import MyBot
 from commands import grind
 
 # Load environment variables from .env file (useful for local development)
@@ -11,19 +11,18 @@ load_dotenv()
 # Get the token from environment variables
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Debugging statement
+print(f"Token: {TOKEN[:5]}...")  # Ensure this prints the actual token (or part of it for security reasons)
+
 # Check if the token is loaded correctly
 if TOKEN is None:
     raise ValueError("DISCORD_TOKEN environment variable is not set")
 
-# Define the bot
+# Define the bot with intents
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
-
-# Register event handlers
-bot.event(on_ready)
-bot.event(on_message)
+bot = MyBot(command_prefix='/', intents=intents)
 
 # Register commands
 bot.command(name='grind')(grind)
