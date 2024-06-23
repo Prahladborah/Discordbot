f075675ec7e52e495635ee031d5cfd42e4d28fd7
Prahladborah <86541744+Prahladborah@users.noreply.github.com>
2024-06-23 22:26:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 if TOKEN is None:
     raise ValueError("DISCORD_TOKEN environment variable is not set")
 
-# Define initial extensions
-initial_extensions = ['cogs.builds', 'cogs.blacksmithing', 'cogs.synthesis', 'cogs.equipment', 'events']
-
 # Define the bot with intents
 intents = discord.Intents.default()
 intents.messages = True
@@ -28,11 +25,37 @@ class MyBot(commands.Bot):
     def __init__(self, command_prefix, **options):
         super().__init__(command_prefix, **options)
         self.state = 'main_menu'
-        for extension in initial_extensions:
-            self.load_extension(extension)
+        
+    async def setup_hook(self):
+        await self.load_extension('cogs.builds')
+        await self.load_extension('cogs.blacksmithing')
+        await self.load_extension('cogs.synthesis')
+        await self.load_extension('cogs.equipment')
+        await self.load_extension('events')
         
     async def on_ready(self):
         print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        content = message.content.lower()
+
+        # Check for greetings and send a response
+        await send_greeting_response(message, content)
+
+        # Check for "potato guide" or "potato bot" and send a response
+        await send_potato_response(message, content)
+
+        # Check for "baka bot" and send a response
+        await send_baka_response(message, content)
+
+        # Initial trigger for help
+        if any(x in content for x in ['is anyone here who can help me', 'i need help', 'someone help me', 'hey guide','elite guide','hey elite']):
+            self.state = await send_help_trigger(message)
+        else:
+            await self.process_commands(message)
 
 # Create the bot instance
 bot = MyBot(command_prefix='/', intents=intents)
