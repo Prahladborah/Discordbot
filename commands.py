9b2d14862f3594e8a36a22a78d2eec3ab77d04d8
Prahladborah <86541744+Prahladborah@users.noreply.github.com>
2024-06-24 10:22:05 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,12 +1,11 @@
 import discord
 from helpers import get_leveling_info
 
-async def grind(ctx, arg=None):
-    if arg is None:
+async def grind(ctx, level: int = None):
+    if level is None:
         await ctx.send("Usage: /grind [level]")
     else:
         try:
-            level = int(arg)
             info = get_leveling_info(level)
             await ctx.send(info)
         except ValueError:
